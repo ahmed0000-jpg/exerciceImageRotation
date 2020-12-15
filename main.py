129747b7e1c5f30925c732b44e716a8e4f612671
Ahmed BELAIBA <ahmedbelaiba19952018@gmail.com>
2020-12-15 01:18:39 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import piexif
+
+def rotate_jpeg(filename):
+    img = Image.open(filename)
+    if "exif" in img.info:
+        exif_dict = piexif.load(img.info["exif"])
+
+        if piexif.ImageIFD.Orientation in exif_dict["0th"]:
+            orientation = exif_dict["0th"].pop(piexif.ImageIFD.Orientation)
+            exif_bytes = piexif.dump(exif_dict)
+
+            if orientation == 2:
+                img = img.transpose(Image.FLIP_LEFT_RIGHT)
+            elif orientation == 3:
+                img = img.rotate(180)
+            elif orientation == 4:
+                img = img.rotate(180).transpose(Image.FLIP_LEFT_RIGHT)
+            elif orientation == 5:
+                img = img.rotate(-90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+            elif orientation == 6:
+                img = img.rotate(-90, expand=True)
+            elif orientation == 7:
+                img = img.rotate(90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+            elif orientation == 8:
+                img = img.rotate(90, expand=True)
+
+            img.save(filename, exif=exif_bytes)
+def main():
+    filename = ""
+    rotate_jpeg(filename)
+if __name__ == '__main__':
+    main()
