b511bf25237664d835936d241fc21ce84a82d641
IT Educator <ahmedbelaiba19952018@gmail.com>
2020-12-23 17:22:52 +0100

    mizalit juste logging

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import logging
 import argparse, os
 import shutil
 from datetime import datetime
+from configparser import ConfigParser
+file = 'config.ini'
+config = ConfigParser()
+config.read(file)
 def dir_path(string):
     if os.path.isdir(string):
         return string
@@ -14,7 +18,7 @@ def dir_path(string):
         raise NotADirectoryError(string)
 def rotate_image(path):
     filename = os.path.basename(path)
-    dst_dir = "backup"
+    dst_dir = config['param']['path_backup']
     src_dir = os.path.dirname(path)
     img = Image.open(path)
     if "exif" in img.info:
@@ -42,14 +46,16 @@ def rotate_image(path):
                 img = img.rotate(90, expand=True)
             img.save(path, exif=exif_bytes)
 def main():
-    logging.basicConfig(filename = f"{datetime.strftime(datetime.now(), '%Y-%m-%d')}.log", filemode='w',level=logging.INFO)
+    #logging.basicConfig(filename = f"{datetime.strftime(datetime.now(), '%Y-%m-%d')}.log", filemode='w',level=logging.INFO)
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', type=dir_path)
     parsed_args = parser.parse_args()
     result = []
     for x in os.walk(parsed_args.path):
-        for y in glob.glob(os.path.join(x[0], '*.jpg')):
-            result.append(y)
+        for y in glob.glob(os.path.join(x[0], '*')):
+            if os.path.isfile(y):
+                result.append(y)
     #pdb.set_trace()
     i = 0
     for i in range(len(result)):
